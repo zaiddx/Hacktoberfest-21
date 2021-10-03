1ffdbe11add1aeb7e3a16909eb91bbde70d9a458
zaiddx <72139621+zaiddx@users.noreply.github.com>
2021-10-03 12:35:41 +0530

    Create prime_number.cpp

---
+++ prime_number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+bool isprime(int n){
+    int d = 2;
+    while(d<n){
+        if (n%d==0){
+            return false;
+        }
+        d++;
+    }
+    return true;    
+}
+int main(){
+    bool ans = isprime(31);
+    cout << ans << endl;
+    ans  = isprime(85);
+    cout << ans << endl;
+    return 0;
+}
