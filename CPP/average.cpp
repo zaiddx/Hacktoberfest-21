c03f9db79d3f3accfceb0284009b12e24a771782
zaiddx <72139621+zaiddx@users.noreply.github.com>
2021-10-04 13:14:18 +0530

    Average.cpp
    Update the program to find average of n numbers

--- CPP/average.cpp
+++ CPP/average.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 using namespace std;
-int main(){
-  cout<<"Enter the number of values: ";
-  double n; cin>>n;
-  cout<<endl;
-  double sum=0;
-  cout<<"Enter the values: ";
-  int a;
-  for(int i=0; i<n; i++){
-    cin>>a;
-    sum+=a;
-  }
-  cout<<endl<<sum/n<<endl;
+
+int main()
+{
+    int n, i;
+    float num[100], sum=0.0, average;
+
+    cout << "Enter the numbers of data: ";
+    cin >> n;
+    while (n > 100 || n <= 0)
+    {
+        cout << "Error! number should in range of (1 to 100)." << endl;
+        cout << "Enter the number again: ";
+        cin >> n;
+    }
+    for(i = 0; i < n; ++i)
+    {
+        cout << i + 1 << ". Enter number: ";
+        cin >> num[i];
+        sum += num[i];
+    }
+    average = sum / n;
+    cout << "Average = " << average;
+    return 0;
 }
